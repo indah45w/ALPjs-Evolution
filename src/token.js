e27a9d6eaa837d96fdb3921a69a6b8cbf5bea9b0
Indah45 <35174281+indah45@users.noreply.github.com>
2018-06-23 03:37:06 +0700

    Update and rename main.js to token.js

---
+++ src/token.js
@@ -0,0 +1,166 @@
+const LineAPI = require('./api');
+const request = require('request');
+const fs = require('fs');
+const unirest = require('unirest');
+const webp = require('webp-converter');
+const path = require('path');
+const rp = require('request-promise');
+const config = require('./config');
+const { Message, OpType, Location } = require('../curve-thrift/line_types');
+
+
+
+const myBot = ['u35a1aa2a15a9fa18452c6845312874f8','uc301fa8f0962f52b1f2d83dc251589cb'];//TARO MID LU DISINI
+var vx = {};var midnornama = "";var pesane = "";var kickhim = "";var waitMsg = "no";//DO NOT CHANGE THIS
+
+
+class LINE extends LineAPI {
+    constructor() {
+        super();
+        this.receiverID = '';
+        this.checkReader = [];
+        this.sendStaff = 0;
+        this.stateStatus = {
+            mute: 0,
+            lockinvite: 0,
+            lockupdategroup: 0,
+            lockjoin: 0,
+            lockcancel: 1,
+            kick:1,
+            cancel: 1,
+            bc: 0,
+            bmsg: 1,
+        }
+    }
+
+    getOprationType(operations) {
+        for (let key in OpType) {
+            if(operations.type == OpType[key]) {
+                if(key !== 'NOTIFIED_UPDATE_PROFILE') {
+                    console.info(`[* ${operations.type} ] ${key} `);
+                }
+            }
+        }
+    }
+
+
+    poll(operation) {
+        if(operation.type == 25 || operation.type == 26) {
+            const txt = (operation.message.text !== '' && operation.message.text != null ) ? operation.message.text : '' ;
+            let message = new Message(operation.message);
+            this.receiverID = message.to = (operation.message.to === myBot[0]) ? operation.message.from : operation.message.to ;
+            Object.assign(message,{ ct: operation.createdTime.toString() });
+            if(waitMsg == "yes" && operation.message.from == vx[0] && this.stateStatus.mute != 1){
+				this.textMessage(txt,message,message.text)
+			}else if(this.stateStatus.mute != 1){this.textMessage(txt,message);
+			}else if(txt == "#unmute" && isAdminOrBot(operation.message.from) && this.stateStatus.mute == 1){
+			    this.stateStatus.mute = 0;
+			    this._sendMessage(message,"BOT ON")
+		    }else{console.info("Bot Off");}
+        }
+
+        if(operation.type == 13 && this.stateStatus.mc == 1) {
+            this.cancelAll(operation.param1);
+        }
+
+        if(operation.type == 19) { //ada kick
+            // op1 = group nya
+            // op2 = yang 'nge' kick
+            // op3 = yang 'di' kick
+            if(isAdminOrBot(operation.param3)) {
+                this.__inviteIntoGroup(operation.param1,[operation.param3]);
+            }
+            if(isAdminOrBot(operation.param2)) {
+                this._kickMember(operation.param1,[operation.param2]);
+            }
+
+        }
+
+        if(operation.type == 55){ //ada reader
+
+            const idx = this.checkReader.findIndex((v) => {
+                if(v.group == operation.param1) {
+                    return v
+                }
+            })
+            if(this.checkReader.length < 1 || idx == -1) {
+                this.checkReader.push({ group: operation.param1, users: [operation.param2], timeSeen: [operation.param3] });
+            } else {
+                for (var i = 0; i < this.checkReader.length; i++) {
+                    if(this.checkReader[i].group == operation.param1) {
+                        if(!this.checkReader[i].users.includes(operation.param2)) {
+                            this.checkReader[i].users.push(operation.param2);
+                            this.checkReader[i].timeSeen.push(operation.param3);
+                        }
+                    }
+                }
+            }
+        }
+
+        if(operation.type == 13) { // diinvite
+            if(isAdminOrBot(operation.param2)) {
+                return this._acceptGroupInvitation(operation.param1);
+            } else {
+                return this._cancel(operation.param1,myBot);
+            }
+        }
+        this.getOprationType(operation);
+    }
+
+    
+
+    async leftGroupByName(payload) {
+        let gid = await this._findGroupByName(payload);
+        for (var i = 0; i < gid.length; i++) {
+            this._leaveGroup(gid[i]);
+        }
+    }
+    
+    async check(cs,group) {
+        let users;
+        for (var i = 0; i < cs.length; i++) {
+            if(cs[i].group == group) {
+                users = cs[i].users;
+            }
+        }
+        
+        let contactMember = await this._getContacts(users);
+        return contactMember.map((z) => {
+                return { displayName: z.displayName, mid: z.mid };
+            });
+    }
+
+    removeReaderByGroup(groupID) {
+        const groupIndex = this.checkReader.findIndex(v => {
+            if(v.group == groupID) {
+                return v
+            }
+        })
+
+        if(groupIndex != -1) {
+            this.checkReader.splice(groupIndex,1);
+        }
+    }
+
+    async textMessage(textMessages, seq) {
+        let [ cmd, ...payload ] = textMessages.split(' ');
+        payload = payload.join(' ');
+        let txt = textMessages.toLowerCase();
+        let messageID = seq.id;
+
+        if(cmd == 'cancel') {
+            if(payload == 'group') {
+                let groupid = await this._getGroupsInvited();
+                for (let i = 0; i < groupid.length; i++) {
+                    this._rejectGroupInvitation(groupid[i]);                    
+                }
+            }
+        }
+
+  
+
+    }
+
+}
+
+module.exports = new LINE();
